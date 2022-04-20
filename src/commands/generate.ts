a2b31880c34f5c04f1a6eba3a9e8090a44ce9f7f
Missy Danielsen <missy.danielsen@gmail.com>
2022-04-20 13:39:49 -0500

    feat(generate): bare bones one page view

--- src/commands/generate.ts
+++ src/commands/generate.ts
@@ -1,4 +1,57 @@
+import sortBy from 'lodash/fp/sortBy';
 import type { GluegunCommand, GluegunToolbox } from 'gluegun';
+import type { TweetEntry } from './scrape';
+
+export function parseText(tweet: TweetEntry) {
+  const urlParts =
+    tweet.entities?.urls?.map(({ start, end, expanded_url, display_url }) => {
+      return {
+        start,
+        end,
+        text: `<a href="${expanded_url}">${display_url}</a>`,
+      };
+    }) ?? [];
+
+  const hashtagParts =
+    tweet.entities?.hashtags?.map(({ start, end, tag }) => {
+      const displayText = tweet.text.substring(start, end);
+      return {
+        start,
+        end,
+        text: `<a href="https://twitter.com/hashtag/${tag}">${displayText}</a>`,
+      };
+    }) ?? [];
+
+  const mentionParts =
+    tweet.entities?.mentions?.map(({ start, end, username }) => {
+      const displayText = tweet.text.substring(start, end);
+      return {
+        start,
+        end,
+        text: `<a href="https://twitter.com/${username}">${displayText}</a>`,
+      };
+    }) ?? [];
+
+  const sortedParts = sortBy('start', [
+    ...urlParts,
+    ...hashtagParts,
+    ...mentionParts,
+  ]);
+
+  const parsedText = sortedParts.reduce((text, part, index) => {
+    const start = index === 0 ? 0 : sortedParts[index - 1].end;
+    const end = part.start;
+    const endText =
+      index === sortedParts.length - 1 ? tweet.text.substring(part.end) : '';
+
+    return text + tweet.text.substring(start, end) + part.text + endText;
+  }, '');
+
+  tweet.text = `<p>${parsedText.replace('\n\n', '</p><p>')}</p>`.replace(
+    '\n',
+    '<br>'
+  );
+}
 
 const command: GluegunCommand = {
   name: 'generate',
@@ -8,19 +61,33 @@ const command: GluegunCommand = {
   run: async (toolbox: GluegunToolbox) => {
     const {
       parameters,
+      filesystem,
       template: { generate },
-      print: { info },
+      print,
     } = toolbox;
 
-    const name = parameters.first || '';
+    // TODO: more options, like specifying language
+    const filename = parameters.first || '';
+    // TODO: account for path
+    const tweetName = filename.substring(0, filename.length - 5);
+
+    const threadJson = filesystem.read(filename);
+    if (!threadJson) {
+      print.error(`Did not find JSON file at ${filename}.`);
+      return;
+    }
+
+    // TODO: type guard with error if it's malformed
+    const thread = JSON.parse(threadJson) as TweetEntry[];
+    thread.forEach(parseText);
 
     await generate({
-      template: 'model.ts.ejs',
-      target: `models/${name}-model.ts`,
-      props: { name },
+      template: 'one-page.ejs',
+      target: `${tweetName}/index.html`,
+      props: { thread },
     });
 
-    info(`Generated file at models/${name}-model.ts`);
+    print.info(`Generated file at ${tweetName}/index.html`);
   },
 };
 
